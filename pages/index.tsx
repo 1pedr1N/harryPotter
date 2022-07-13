2bfe3181f2d9fbb609e84776f2b5ebf10a5b3332
Pedro Henrique <pedrinfnn@gmail.com>
2022-07-13 00:13:33 -0300

    :white_check_mark:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
 import Harry from "./harryMainPage";
-
+import Head from "next/head";
 const Home: NextPage = () => {
-  return <Harry />;
+  return (
+    <div>
+      <Head>
+        <title>Harry Characters</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Harry />
+    </div>
+  );
 };
 
 export default Home;
